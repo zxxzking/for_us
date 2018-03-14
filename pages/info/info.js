6d8d24466003c424ac922e692c70cfae1c2dd9dd
吕泉 <lvquan@paicaifu.com>
2018-03-14 17:58:33 +0800

    some change

--- pages/info/info.js
+++ pages/info/info.js
@@ -1,6 +1,6 @@
 // pages/info/info.js
 const app = getApp()
-
+const currentUser = {}
 
 Page({
 
@@ -10,7 +10,8 @@ Page({
   data: {
     userInfo:{},
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    onlineNum:0
+    onlineNum:0,
+    isShow:true
   },
 
   /**
@@ -18,7 +19,6 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
-
 
     if (app.globalData.userInfo) {
       this.setData({
@@ -32,111 +32,113 @@ Page({
           this.setData({
             userInfo: res.userInfo
           })
-          login();
+          doLogin();
         }
       })      
     }
 
-    
 
-
-
-
-
-
-    function login(){
-      wx.login({
-        success: function (res) {
-          var code = res.code
-          wx.request({
-            url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
-            data: {
-              appid: 'wxb5c7e76760f4bd27',
-              secret: '9473de08dd16a85b7738a96af4c611fe',
-              js_code: code,
-              grant_type: 'authorization_code'
-            },
-            success: function (res) {
-              app.globalData.openId = res.data.openid
-              wx.request({
-                header: {
-                  'content-type': 'application/x-www-form-urlencoded' // 默认值
-                },
-                method: 'post',
-                url: 'http://localhost:9093/wechat/initUser',
-                data: {
-                  openId: res.data.openid,
-                  nickName: app.globalData.userInfo.nickName,
-                  avatarUrl: app.globalData.userInfo.avatarUrl
-                },
-                success: function (res) {
-                  app.globalData.userToken = res.data.data
-                  connect();
-                  
-                }
-              })
-            }
-          })
-        }
-      })
-    }
-
-
-    function connect(){
-      wx.connectSocket({
-        url: 'ws://localhost:9093/zxxz-socket'
-      })
-
-      // 监听socket打开
-      wx.onSocketOpen(function (res) {
-        app.globalData.socketOpen = true;
-        console.log('WebSocket连接已打开！')
-
-        if (app.globalData.socketOpen) {
-          wx.sendSocketMessage({
-            data: [app.globalData.userToken]
-          })
-        } 
-
-      })
-
-      // 监听接收到服务器消息
-      wx.onSocketMessage(function (res) {
-        console.log('收到服务器内容：' + res.data)
-        that.setData({
-          onlineNum:res.data
+    function doLogin(){
+        wx.login({
+          success: function (res) {
+            var code = res.code
+            wx.request({
+              url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
+              data: {
+                appid: 'wxb5c7e76760f4bd27',
+                secret: '9473de08dd16a85b7738a96af4c611fe',
+                js_code: code,
+                grant_type: 'authorization_code'
+              },
+              success: function (res) {
+                app.globalData.openId = res.data.openid
+                wx.request({
+                  header: {
+                    'content-type': 'application/x-www-form-urlencoded' // 默认值
+                  },
+                  method: 'post',
+                  url: 'http://192.168.65.103:9093/wechat/initUser',
+                  data: {
+                    openId: res.data.openid,
+                    nickName: app.globalData.userInfo.nickName,
+                    avatarUrl: app.globalData.userInfo.avatarUrl
+                  },
+                  success: function (res) {
+                    app.globalData.userToken = res.data.data
+                
+                  }
+                })
+              }
+            })
+          }
         })
-      })
-
     }
+
+    // 监听socket关闭
+    wx.onSocketClose(function (res) {
+      app.globalData.socketOpen = false;
+      that.setData({
+        isShow:true
+      })
+      console.log('WebSocket 已关闭！')
+    })
+
+    // 监听socket打开
+    wx.onSocketOpen(function (res) {
+      app.globalData.socketOpen = true;
+      that.setData({
+        isShow:false
+      })
+      console.log('WebSocket连接已打开！')
+
+
+    })
+
+    // 监听接收到服务器消息
+    wx.onSocketMessage(function (res) {
+      console.log('收到服务器内容：' + res.data)
+
+      var resp = JSON.parse(res.data)
+      console.log(resp)
+      that.setData({
+        onlineNum: resp.onlineNum
+      })
+    })
+
+
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+    if (!app.globalData.socketOpen) {
+      wx.connectSocket({
+        url: 'ws://192.168.65.103:9093/zxxz-socket'
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    wx.closeSocket()
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
   
 
@@ -148,13 +150,31 @@ Page({
   },
 
   join:function(){
-      wx.getLocation({
-        success: function(res) {
-
-        },
+    if (!app.globalData.socketOpen){
+      wx.connectSocket({
+        url: 'ws://192.168.65.103:9093/zxxz-socket'
       })
+    }else{
+      currentUser['nickName'] = app.globalData.userInfo.nickName
+      console.log(app.globalData.userInfo)
+      wx.sendSocketMessage({
+        data: [currentUser],
+      })
+    }
+    
 
+   
+
+
+  },
+
+  leave:function(){
+    wx.closeSocket()
 
 
   }
+
+
+
+  
 })
